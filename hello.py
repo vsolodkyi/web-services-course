814f3ceecd032063b57e60b4a29501f830f9b817
vsolodkyi <vsolodkyi@gmail.com>
2020-07-04 08:30:38 +0300

    version with photos from applications

--- hello.py
+++ hello.py
@@ -37,6 +37,7 @@ import joblib
 from sklearn import datasets
 
 knn = joblib.load('knn.pkl')
+
 @app.route('/iris/<params>')
 
 
@@ -115,7 +116,7 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = ''
+UPLOAD_FOLDER = './files/photo'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 #app = Flask(__name__)
@@ -151,3 +152,84 @@ def upload_file():
       <input type=submit value=Upload>
     </form>
     '''
+
+UPLOAD_FOLDER_PHOTO = './static/photos'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'csv'}
+
+#app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER_PHOTO
+
+#from Pillow import Image
+import tensorflow as tf
+#from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+model_vgg16 = tf.keras.models.load_model('supergypsy.h5')
+
+dict_indices = {0: 'ATTENTION! Client looks like Gypsy King', 
+                1:'Your client looks like a good one'} 
+
+@app.route('/check_good_bad', methods=['GET', 'POST'])
+
+
+
+#from matplotlib.image import imread
+
+
+def upload_check_photo():
+    #print(request.method)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        print(file.filename)
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #print(filename)
+            imgnew = image.load_img(os.path.join(app.config['UPLOAD_FOLDER'], filename), target_size=(150, 150))
+            imgnew_array = image.img_to_array(imgnew)
+
+            pred = model_vgg16.predict(np.expand_dims(imgnew_array, axis=0))
+            pred_int = 0 if pred <=0.5 else 1
+            #print('Our client looks like - ' + str(dict_indices[pred_int]))
+
+            #print(pred[0][0])
+            stroka = '<h2>'+ str(dict_indices[pred_int]) + ', рейтинг фото - '+ str(round(pred[0][0],3)) + '</h2> <img src="' + UPLOAD_FOLDER_PHOTO + '/' + filename+ '" alt="Image">'
+            return stroka
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+import glob
+
+from glob import glob
+FOLDER_IMG = './static/img'
+
+@app.route('/check_model', methods=['GET'])
+def model_check():
+    sentence = ''
+    for img_name in glob(FOLDER_IMG+'/*.jpg')+glob(FOLDER_IMG+'/*.png'):
+        #print(img_name)
+        imgnew = image.load_img(img_name, target_size=(150, 150))
+        imgnew_array = image.img_to_array(imgnew)
+        pred = model_vgg16.predict(np.expand_dims(imgnew_array, axis=0))
+        pred_int = 0 if pred <=0.5 else 1
+        #stroka = '<h2>'+ str(dict_indices[pred_int]) + '</h2> <img src="' + img_name+ '" alt="Image">'
+        stroka = '<h2>'+ str(dict_indices[pred_int]) + ', рейтинг фото - '+ str(round(pred[0][0],3)) + '</h2> <img src="' + img_name + '" alt="Image">'
+        sentence = sentence + stroka
+    return sentence
+ 
+
